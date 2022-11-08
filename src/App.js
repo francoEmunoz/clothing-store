ede077b47c29757a2d6dd82089bdcda781677074
francoEmunoz <munozfranco011@gmail.com>
2022-11-08 18:46:27 -0300

    importo paginas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
 import WebsiteLayout from './layouts/WebsiteLayout';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <WebsiteLayout>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </WebsiteLayout>
     </BrowserRouter>
