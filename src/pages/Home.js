35a2a8aa823f51e35ca65bd3355184dcfed25732
francoEmunoz <munozfranco011@gmail.com>
2022-11-08 18:47:44 -0300

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import { Link as LinkRouter } from "react-router-dom";
 import '../styles/Home.css';
 
 export default function Home() {
@@ -22,6 +24,7 @@ export default function Home() {
                         alt="Second slide"
                     />
                     <Carousel.Caption>
+                        <h3>SHOES</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -31,54 +34,12 @@ export default function Home() {
                         alt="Third slide"
                     />
                     <Carousel.Caption>
+                        <h3>JACKETS</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            <Carousel className='carousel-home'>
-                <Carousel.Item interval={1000}>
-                    <img
-                        className="d-block w-100"
-                        src="dripping.jpg"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block w-100"
-                        src="shoes.png"
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="Accesorios.jpg"
-                        alt="Third slide"
-                        width="33%"
-                    />
-                    <img
-                        className="d-block w-100"
-                        src="Accesorios.jpg"
-                        alt="Third slide"
-                        width="33%"
-                    />
-                    <img
-                        className="d-block w-100"
-                        src="Accesorios.jpg"
-                        alt="Third slide"
-                        width="33%"
-                    />
-                    <Carousel.Caption>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-            <img src="img-main.jpg" alt="" width="88%"></img>
-            <div className='our-products'>
-                <h2>Our Products</h2>
+            <div className='call-to-action'>
+            <LinkRouter to="/products"><Button variant="warning" className='btn-action'>Go to Products</Button></LinkRouter>
             </div>
         </div>
     )
