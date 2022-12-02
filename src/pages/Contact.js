2adb2ae0556afa2ced6a4e6c3ae4b34175ab8e96
francoEmunoz <munozfranco011@gmail.com>
2022-12-02 20:49:47 -0300

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import '../styles/Contact.css'
+
+function Input({ label, name, type }) {
+  return (
+    <label className="form-label">
+      {label}
+      <input name={name} type={type} />
+    </label>
+  );
+}
 
 export default function Contact() {
+
+  const form = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const formUser = {
+      name: formData.get("name"),
+      mail: formData.get("mail"),
+      message: formData.get("message"),
+      affair: formData.get("affair"),
+    };
+
+    form.current.reset();
+  };
+
   return (
-    <div>Contact</div>
+    <div className='contact-body'>
+      <img src="contact.jpg" alt="contact"></img>
+      <div>
+        <h3>Contact</h3>
+        <p>Do you have any questions, do you want to leave your products on consignment or place an order? Contact Us.</p>
+        <form className='form' ref={form}>
+          <div className='sign-in-input-container'>
+            <Input label="Name:" name="name" />
+            <Input label="Lastname:" name="lastname" />
+            <Input label="Mail:" name="mail" />
+            <Input label="Country:" name="country" />
+            <div className='btn-login'>
+              <button className='btn-action' onClick={handleSubmit}>Login</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
